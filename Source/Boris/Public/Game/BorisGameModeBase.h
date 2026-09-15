b4fc0dfd0b5cb85bafe61bb163d506b5e7fba437
AdvaitKhot003 <adwaitkhot@gmail.com>
2026-09-15 23:02:37 +0530

    Setup Boris GameMode

---
+++ Source/Boris/Public/Game/BorisGameModeBase.h
@@ -0,0 +1,16 @@
+﻿// No Copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BorisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BORIS_API ABorisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
